9845bd685a77115aeb8f7e39dc49b98a6dd54f37
ankit-suman-07 <ankitsuman07@gmail.com>
2023-08-01 01:44:48 +0530

    Added sort menu option

--- src/pages/Shop/Shop.jsx
+++ src/pages/Shop/Shop.jsx
@@ -2,6 +2,9 @@ import React, { useState } from "react";
 import { PRODUCTS } from "../../helper/Products";
 import SearchIcon from '@mui/icons-material/Search';
 import { Product } from "./Product";
+import SouthIcon from '@mui/icons-material/South';
+import NorthIcon from '@mui/icons-material/North';
+import SortIcon from '@mui/icons-material/Sort';
 import "../../css/Shop.css";
 
 export const Shop = () => {
@@ -9,80 +12,122 @@ export const Shop = () => {
   const [searchFilter, setSearchFilter] = useState("");
   const [activeButton, setActiveButton] = useState("all");
   const [searchKeyword, setSearchKeyword] = useState("");
+  const [sortArray, setSortedArray] = useState(PRODUCTS);
+  const [performSort, setPerformSort] = useState(false);
 
-    const handleFilterChange = (newFilter) => {
-        setFilter(newFilter);
-        setActiveButton(newFilter);
-    };
+  const handlePerformSort = () => {
+    setPerformSort((prevPerformSort) => !prevPerformSort);
+  };
 
-    const handleSearch = () => {
-      setSearchFilter(searchKeyword); // Step 3: Update filter with search keyword
-    };
+  const sortAscendingAlpha = () => {
+    const sortedData = [...sortArray].sort((a, b) => a.productName.localeCompare(b.productName));
+    setSortedArray(sortedData);
+    setPerformSort(false);
+  };
 
+  const sortDescendingAlpha = () => {
+    const sortedData = [...sortArray].sort((a, b) => b.productName.localeCompare(a.productName));
+    setSortedArray(sortedData);
+    setPerformSort(false);
+  };
+
+  const sortAscendingNumeric = () => {
+    const sortedData = [...sortArray].sort((a, b) => a.price - b.price);
+    setSortedArray(sortedData);
+    setPerformSort(false);
+  };
+
+  const sortDescendingNumeric = () => {
+    const sortedData = [...sortArray].sort((a, b) => b.price - a.price);
+    setSortedArray(sortedData);
+    setPerformSort(false);
+  };
+
+  const handleFilterChange = (newFilter) => {
+    setFilter(newFilter);
+    setActiveButton(newFilter);
+  };
+
+  const handleSearch = () => {
+    setSearchFilter(searchKeyword); // Step 3: Update filter with search keyword
+  };
 
   return (
     <div className="shop-page">
       <div className="filter-search" >
         <div className="sort-btn" >
+          <button  className="sort-menu" onClick={handlePerformSort} >
+            <SortIcon />
+          </button>
+          
           <button
             className={activeButton === "all" ? "active-filter" : "all"}
             onClick={() => handleFilterChange("all")}
-            >
-              All
+          >
+            All
           </button>
           <button
             className={activeButton === "jet" ? "active-filter" : "jet"}
-              onClick={() => handleFilterChange("jet")}
-              >
-              Jet
+            onClick={() => handleFilterChange("jet")}
+          >
+            Jet
           </button>
           <button
             className={activeButton === "island" ? "active-filter" : "island"}
-              onClick={() => handleFilterChange("island")}
-              >
-              island
+            onClick={() => handleFilterChange("island")}
+          >
+            Island
           </button>
           <button
             className={activeButton === "yacht" ? "active-filter" : "yacht"}
-              onClick={() => handleFilterChange("yacht")}
-              >
-              Yacht
+            onClick={() => handleFilterChange("yacht")}
+          >
+            Yacht
           </button>
           <button
             className={activeButton === "rare" ? "active-filter" : "rare"}
-              onClick={() => handleFilterChange("rare")}
-              >
-              Rare
+            onClick={() => handleFilterChange("rare")}
+          >
+            Rare
           </button>
         </div>
         <div className="search-div">
           <input
             type="text"
-            value={searchKeyword} 
-            onChange={(e) => setSearchKeyword(e.target.value)} 
+            value={searchKeyword}
+            onChange={(e) => setSearchKeyword(e.target.value)}
             className="search-text"
           />
-          <button onClick={handleSearch} className="search-btn" >
-            <SearchIcon  className="search-icon" />  
-          </button> 
+          <button onClick={handleSearch} className="search-btn">
+            <SearchIcon className="search-icon" />
+          </button>
+          {
+            (performSort &&
+              <div className="all-sorts" >
+              <button onClick={sortAscendingAlpha} className="all-sort-btn" >A - Z </button>
+              <button onClick={sortDescendingAlpha} className="all-sort-btn" >Z - A</button>
+              <button onClick={sortAscendingNumeric} className="all-sort-btn" >Price  <NorthIcon /> </button>
+              <button onClick={sortDescendingNumeric} className="all-sort-btn" >Price  <SouthIcon /> </button>
+            </div>
+              )
+          }
+          
         </div>
       </div>
       <div className="products">
-        {
-          
-          PRODUCTS.map((product) => {
-            const lowerCaseFilter = searchFilter.toLowerCase();
-            const lowecasename = (product.productName).toLowerCase();
-            if((product.tags).includes(filter) && lowecasename.includes(lowerCaseFilter)) {
-                return (
-                  <Product data={product} />
-                );
-            }
-            else {
-              return null;
-            }
-          
-          })}
+        {performSort
+          ? PRODUCTS.map((product) => {
+              const lowerCaseFilter = searchFilter.toLowerCase();
+              const lowerCaseName = product.productName.toLowerCase();
+              if (product.tags.includes(filter) && lowerCaseName.includes(lowerCaseFilter)) {
+                return <Product data={product} />;
+              } else {
+                return null;
+              }
+            })
+          : sortArray.map((product) => {
+              return <Product data={product}  />;
+            })}
       </div>
     </div>
   );
